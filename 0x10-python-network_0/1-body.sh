20ac7d0bc18043a915c56d6638e43079bcbb2f23
chinedu06 <godswillchinedu644@gmail.com>
2024-07-09 08:05:58 -0700

    first commit

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a URL and displays the body of the response if the status code is 200
-curl -s -o - -w "%{http_code}" "$1" | awk '/200$/{print x};{x=$0}'
-
+# Script that sends a GET request to a URL and displays the body of a 200 status code response
+curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1"
