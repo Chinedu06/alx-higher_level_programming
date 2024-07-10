64b598cc63012ca0cc68e4418c438e5883692032
chinedu06 <godswillchinedu644@gmail.com>
2024-07-10 04:56:53 -0700

    first commit

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
 """
-Script to send a POST request to a URL with an email as a parameter
+Takes URL and email sends a POST request with the email and displays response.
 """
 import requests
 import sys
 
-def send_post_request(url, email):
-    """
-    Send a POST request to the provided URL with the email as a parameter
-    and display the body of the response
-    """
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
     response = requests.post(url, data={'email': email})
     print(response.text)
-
-if __name__ == "__main__":
-    if len(sys.argv) > 2:
-        send_post_request(sys.argv[1], sys.argv[2])
